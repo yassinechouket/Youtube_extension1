461e259638cdde1ebec0c3f6dcbf2687abdfb39e
yassinechouket <medyassinechouket.2@gmail.com>
2025-10-24 23:58:41 +0100

    bookmart ytb

--- contentScript.js
+++ contentScript.js
@@ -10,4 +10,17 @@
                 newVideoLoaded();
             }
         })
+
+    const newVideoLoaded =() =>{
+        const bookmarkBtnExists =document.getElementsByClassName("bookmark-btn")[0];
+
+        if(bookmarkBtnExists){
+            const bookmarkBtn=document.createElement("img");
+
+            bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
+            bookmarkBtn.className = "ytp-button " + "bookmark-btn";
+            bookmarkBtn.title = "Click to bookmark current timestamp";
+
+        }
+    }
 })();
